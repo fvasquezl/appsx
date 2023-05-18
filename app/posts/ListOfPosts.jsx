74f7a83539bd6fceff538cdfa5448edfda1f94de
Faustino Vasquez Limon <vasquez.limon@gmail.com>
2023-05-18 16:02:13 -0700

    first commit

---
+++ app/posts/ListOfPosts.jsx
@@ -0,0 +1,23 @@
+import Link from "next/link"
+import LikeButton from "./LikeButton"
+
+const fetchPosts = () => {
+    return fetch('https://jsonplaceholder.typicode.com/posts', {
+        next: {
+            revalidate: 60
+        }
+    }).then(res => res.json())
+}
+export async function ListOfPosts() {
+    const posts = await fetchPosts()
+
+    return posts.slice(0, 5).map(post => (
+        <article key={post.id}>
+            <Link href="/posts/[id]" as={`/posts/${post.id}`} >
+                <h2>{post.title}</h2>
+                <p>{post.body}</p>
+                <LikeButton id={post.id} />
+            </Link>
+        </article>
+    ))
+}
---
+++ app/posts/page.jsx
@@ -0,0 +1,10 @@
+import { ListOfPosts } from "./ListOfPosts"
+
+export default async function PostPage() {
+
+    return (
+        <section>
+            <ListOfPosts />
+        </section>
+    )
+}
